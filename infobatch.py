f5cdd0bab6f162ffe0eca7205db1f01359fff2d5
henryqin1997 <henryqin1997@gamil.com>
2023-07-19 16:40:39 +0800

    update data loading to infobatch

--- infobatch.py
+++ infobatch.py
@@ -172,7 +172,7 @@ class DistributedSamplerWrapper(DistributedSampler):
         self,
         sampler,
         num_replicas: Optional[int] = torch.cuda.device_count(),
-        rank: Optional[int] = int(os.environ['LOCAL_RANK']),
+        rank: Optional[int] = os.environment['GLOBAL_RANK'],
         shuffle: bool = True,
     ):
         """

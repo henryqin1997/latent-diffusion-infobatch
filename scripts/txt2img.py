383ae262a909971df88e08f18fa554abd83f7151
henryqin1997 <henryqin1997@gamil.com>
2023-08-03 16:06:14 +0800

    update first_stage_model path

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -105,7 +105,8 @@ if __name__ == "__main__":
 
 
     config = OmegaConf.load("configs/latent-diffusion/txt2img-1p4B-eval.yaml")  # TODO: Optionally download from same location as ckpt and chnage this logic
-    model = load_model_from_config(config, "models/ldm/text2img-large/model.ckpt")  # TODO: check path
+#     model = load_model_from_config(config, "models/ldm/text2img-large/model.ckpt")  # TODO: check path
+    model = load_model_from_config(config, "logs/2023-07-25T10-50-22_cin-ldm-vq-f8-infobatch/checkpoints/last.ckpt")
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model = model.to(device)
